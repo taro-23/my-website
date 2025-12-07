db3e6547c3bea4e46b230ba57f49684e293fa3d5
taro-23 <torakimaru23@gmail.com>
2025-12-08 02:11:00 +0900

    add google tag

--- my-site/astro.config.mjs
+++ my-site/astro.config.mjs
@@ -9,9 +9,16 @@ import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
 
+import partytown from '@astrojs/partytown';
+
+
 export default defineConfig({
   site: "https://kimurataro.com/",
-  integrations: [react(), mdx(), icon(), sitemap()],
+  integrations: [react(), mdx(), icon(), sitemap(), partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })],
   vite: {
     plugins: [tailwindcss()],
   },
